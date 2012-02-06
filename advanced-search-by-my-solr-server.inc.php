8a4cb5759d37e2ec54e65173efec6724c251ce61
Dominique Bejean <dominique.bejean@eolya.fr>
2012-02-06 23:37:37 +0100

    version 2

--- advanced-search-by-my-solr-server.inc.php
+++ advanced-search-by-my-solr-server.inc.php
@@ -2,25 +2,27 @@
 /*
  Copyright (c) 2011 www.mysolrserver.com
 
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to deal
- in the Software without restriction, including without limitation the rights
- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Software, and to permit persons to whom the Software is
- furnished to do so, subject to the following conditions:
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
- The above copyright notice and this permission notice shall be included in
- all copies or substantial portions of the Software.
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
 
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- THE SOFTWARE.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
 */
 require_once("lib/std.encryption.class.inc.php");
+require_once("SolrPhpClient/Apache/Solr/Service.php");
+require_once("solr.class.inc.php");
 
 $bDev = false;
 if ($bDev) {
@@ -32,78 +34,333 @@ else {
 	$url_extraparam = "";
 }
 $url_mysolrserver .= '/mysolrserver_ws/manager.php';
+$this_plugin_dir_url = plugin_dir_url("") . 'advanced-search-by-my-solr-server/';
+
+
+function mss_startswith($str, $sub) {
+	return ( substr( $str, 0, strlen( $sub )) === $sub );
+}
+function mss_endswith($str, $sub) {
+	return ( substr( $str, strlen( $str ) - strlen( $sub )) === $sub );
+}
 
 function encrypt($value) {
 	$crypt = new encryption_class;
 	return $crypt->encrypt("mysolrserver", $value, strlen($value));
 }
+
 function decrypt($value) {
 	$crypt = new encryption_class;
 	return $crypt->decrypt("mysolrserver", $value);
 
 }
 
+function mss_get_option() {
+	return get_option('plugin_mss_settings');
+}
+
+function mss_update_option($optval) {
+	update_option('plugin_mss_settings', $optval);
+}
+
 function POSTGET($param){
 	if (isset($_POST[$param]) && $_POST[$param]!="")
-		return $_POST[$param];
+	return $_POST[$param];
 	if (isset($_GET[$param]) && $_GET[$param]!="")
-		return $_GET[$param];
+	return $_GET[$param];
 	return "";
 }
 
-function check_w4s() {
-	if (get_option("s4w_solr_initialized") != '1') {
-
-		update_site_option('s4w_index_all_sites', '0');
-		update_option('s4w_solr_host', 'localhost');
-		update_option('s4w_solr_port', '8983');
-		update_option('s4w_solr_path', '/solr');
-		update_option('s4w_index_pages', '1');
-		update_option('s4w_index_posts', '1');
-		update_option('s4w_delete_page', '1');
-		update_option('s4w_delete_post', '1');
-		update_option('s4w_private_page', '1');
-		update_option('s4w_private_post', '1');
-		update_option('s4w_output_info', '1');
-		update_option('s4w_output_pager', '1');
-		update_option('s4w_output_facets', '1');
-		update_option('s4w_exclude_pages', ''); 
-		update_option('s4w_num_results', '5');
-		update_option('s4w_cat_as_taxo', '1');
-		update_option('s4w_solr_initialized', '1');
-		update_option('s4w_max_display_tags', '10');
-		update_option('s4w_facet_on_categories', '1');
-		update_option('s4w_facet_on_tags', '1');
-		update_option('s4w_facet_on_author', '1');
-		update_option('s4w_facet_on_type', '1');
-		update_option('s4w_enable_dym', '1');
-		update_option('s4w_index_comments', '1');
-		update_option('s4w_connect_type', 'solr');
-		update_option('s4w_index_custom_fields', ''); 
-		update_option('s4w_facet_on_custom_fields', 'NA'); 
-
-		//		<p>It seems that <strong>Solr for Wordpress plugin</strong> is not initialized. Prior to continue with <strong>My Solr Server plugin</strong> configuration, install
-		//		<a href='http://wordpress.org/extend/plugins/solr-for-wordpress/' target='_mss'>Solr for Wordpress plugin</a> then go at least once in <strong>Solr Options</strong> page.
-		//	</div>
-		//</div>
-		//</div>
-	}
-}
-
-function getAccountInfo($url_mysolrserver, $url_extraparam, $mss_id, $mss_passwd) {
+function getMssAccountInfo($url_mysolrserver, $url_extraparam, $mss_id, $mss_passwd) {
 	$url = $url_mysolrserver . '?action=accountgetinfo&name=' . $mss_id . '&passwd=' . $mss_passwd . '&type=wp' . $url_extraparam;
-	log_message("getAccountInfo - url = " . $url_mysolrserver);
+	log_message("getMssAccountInfo - url = " . $url_mysolrserver);
 	$json = file_get_contents($url);
-	log_message("getAccountInfo - json = " . $json);
+	log_message("getMssAccountInfo - json = " . $json);
 	return $json;
 }
 
 function log_message($message) {
-    if (WP_DEBUG === true) {
-        if (is_array($message) || is_object($message)) {
-            error_log(print_r($message, true));
-        } else {
-            error_log($message);
-        }
-    }
+	if (WP_DEBUG === true) {
+		if (is_array($message) || is_object($message)) {
+			error_log(print_r($message, true));
+		} else {
+			error_log($message);
+		}
+	}
 }
+
+/*
+ * Indexing functions
+*/
+
+function mss_load_all($options, $prev) {
+	global $wpdb;
+	$documents = array();
+	$cnt = 0;
+	$batchsize = 100;
+	$last = "";
+	$found = FALSE;
+	$end = FALSE;
+	$percent = 0;
+
+	$post_type = $options['mss_post_types'];
+	if ($post_type=='') {
+		printf("{\"last\": \"0\", \"end\": true, \"percent\": \"100\"}");
+		return;
+	}
+	$aPostType=explode(',', $post_type);
+	$wherePostType = '';
+	for ($i=0;$i<count($aPostType);$i++) {
+		if ($wherePostType!='') $wherePostType .= ' OR ';
+		$wherePostType .= " post_type = '" .  $aPostType[$i] . "'";
+	}
+
+	$posts = $wpdb->get_results("SELECT ID FROM $wpdb->posts WHERE post_status = 'publish' AND ($wherePostType) ORDER BY ID;" );
+	$postcount = count($posts);
+	for ($idx = 0; $idx < $postcount; $idx++) {
+		$postid = $posts[$idx]->ID;
+		$last = $postid;
+		$percent = (floatval($idx) / floatval($postcount)) * 100;
+		if ($prev && !$found) {
+			if ($postid === $prev) {
+				$found = TRUE;
+			}
+			continue;
+		}
+
+		if ($idx === $postcount - 1) {
+			$end = TRUE;
+		}
+
+		$documents[] = mss_build_document($options, get_post($postid) );
+		$cnt++;
+		if ($cnt == $batchsize) {
+			mss_post( $options, $documents, FALSE, FALSE);
+			$cnt = 0;
+			$documents = array();
+			break;
+		}
+	}
+
+	if ( $documents ) {
+		mss_post( $options, $documents , FALSE, FALSE);
+	}
+
+	if ($end) {
+		mss_post($options, FALSE, TRUE, FALSE);
+		printf("{\"last\": \"%s\", \"end\": true, \"percent\": \"%.2f\"}", $last, $percent);
+	} else {
+		printf("{\"last\": \"%s\", \"end\": false, \"percent\": \"%.2f\"}", $last, $percent);
+	}
+}
+
+function mss_build_document( $options, $post_info ) {
+	global $current_site, $current_blog;
+	$doc = NULL;
+	$exclude_ids = $options['mss_exclude_pages'];
+	$categoy_as_taxonomy = $options['mss_cat_as_taxo'];
+	$index_comments = $options['mss_index_comments'];
+
+	if ($post_info) {
+		// check if we need to exclude this document
+		if (in_array($post_info->ID, (array)$exclude_ids) ) {
+			return NULL;
+		}
+		$doc = new Apache_Solr_Document();
+		$auth_info = get_userdata( $post_info->post_author );
+
+		$doc->setField( 'id', $post_info->ID );
+		$doc->setField( 'permalink', get_permalink( $post_info->ID ) );
+		$doc->setField( 'wp', 'wp');
+
+		$numcomments = 0;
+		if ($index_comments) {
+			$comments = get_comments("status=approve&post_id={$post_info->ID}");
+			foreach ($comments as $comment) {
+				$doc->addField( 'comments', $comment->comment_content );
+				$numcomments += 1;
+			}
+		}
+
+		$doc->setField( 'title', $post_info->post_title );
+		$doc->setField( 'content', strip_tags($post_info->post_content) );
+		$doc->setField( 'numcomments', $numcomments );
+		$doc->setField( 'author', $auth_info->display_name );
+		$doc->setField( 'author_s', get_author_posts_url($auth_info->ID, $auth_info->user_nicename));
+		$doc->setField( 'type', $post_info->post_type );
+		$doc->setField( 'date', mss_format_date($post_info->post_date_gmt) );
+		$doc->setField( 'modified', mss_format_date($post_info->post_modified_gmt) );
+		$doc->setField( 'displaydate', $post_info->post_date );
+		$doc->setField( 'displaymodified', $post_info->post_modified );
+
+		$categories = get_the_category($post_info->ID);
+		if ( ! $categories == NULL ) {
+			foreach( $categories as $category ) {
+				if ($categoy_as_taxonomy) {
+					$doc->addField('categories', get_category_parents($category->cat_ID, FALSE, '^^'));
+				} else {
+					$doc->addField('categories', $category->cat_name);
+				}
+			}
+		}
+
+		$tags = get_the_tags($post_info->ID);
+		if ( ! $tags == NULL ) {
+			foreach( $tags as $tag ) {
+				$doc->addField('tags', $tag->name);
+			}
+		}
+			
+		// custom taxonomies
+		$taxo = $options['mss_custom_taxonomies'];
+		$aTaxo = explode(',', $taxo);
+		$taxonomies = (array)get_taxonomies(array('_builtin'=>FALSE),'names');
+		foreach($taxonomies as $parent) {
+			if (in_array($parent, $aTaxo)) {
+				$terms = get_the_terms( $post_info->ID, $parent );
+				if ((array) $terms === $terms) {
+					$parent =  strtolower(str_replace(' ', '_', $parent));		
+					foreach ($terms as $term) {
+						$doc->addField($parent . '_str', $term->name);
+						$doc->addField($parent . '_srch', $term->name);
+					}
+				}
+			}
+		}
+
+		// custom fields
+		$custom = $options['mss_custom_fields'];
+		$aCustom = explode(',', $custom);
+		if (count($aCustom)>0) {
+			if (count($custom_fields = get_post_custom($post_info->ID))) {
+				foreach ((array)$aCustom as $field_name ) {
+					$field = (array)$custom_fields[$field_name];
+					$field_name =  strtolower(str_replace(' ', '_', $field_name));
+					foreach ( $field as $key => $value ) {
+						$doc->addField($field_name . '_str', $value);
+						$doc->addField($field_name . '_srch', $value);
+					}
+				}
+			}
+		}
+
+	} else {
+		_e('Post Information is NULL', 'solrmss');
+	}
+
+	return $doc;
+}
+
+function mss_format_date( $thedate ) {
+	$datere = '/(\d{4}-\d{2}-\d{2})\s(\d{2}:\d{2}:\d{2})/';
+	$replstr = '${1}T${2}Z';
+	return preg_replace($datere, $replstr, $thedate);
+}
+
+function mss_post( $options, $documents, $commit = true, $optimize = false) {
+	try {
+		$solr = new Mss_Solr();
+		if ($solr->connect($options, true)) {
+
+			if ($documents) {
+				$solr->addDocuments( $documents );
+			}
+
+			if ($commit) {
+				$solr->commit();
+			}
+
+			if ($optimize) {
+				$solr->optimize();
+			}
+		}
+	} catch ( Exception $e ) {
+		echo $e->getMessage();
+	}
+}
+
+/*
+ * Search functions
+*/
+function mss_query( $qry, $offset, $count, $fq, $sortby, $options) {
+	$response = NULL;
+	$facet_fields = array();
+	//$options = mss_get_option();
+
+	$solr = new Mss_Solr();
+	if ($solr->connect($options, true)) {
+
+		$facets = $options['mss_facets'];
+		$aFacets = explode(',', $facets);
+		
+		foreach($aFacets as $facet_field) {
+			$facet_field_add = $facet_field . "_str";
+			if ($facet_field=='category') $facet_field_add = 'categories';
+			if ($facet_field=='tag') $facet_field_add = 'tags';
+			if ($facet_field=='author') $facet_field_add = 'author';
+			if ($facet_field=='type') $facet_field_add = 'type';
+			$facet_field_add =  strtolower(str_replace(' ', '_', $facet_field_add));
+			$facet_fields[] = $facet_field_add;
+		}
+
+		$params = array();
+		$params['defType'] = 'dismax';
+		$params['qf'] = 'tagssrch^5 title^10 categoriessrch^5 content^3.5 comments^1.5'; // TODO : Add "_srch" custom fields ?
+		$params['pf'] = 'title^15 text^10';
+		$params['facet'] = 'true';
+		$params['facet.field'] = $facet_fields;
+		$params['facet.mincount'] = '1';
+		$params['fq'] = $fq;
+		$params['fl'] = '*,score';
+		$params['hl'] = 'on';
+		$params['hl.fl'] = 'content';
+		$params['hl.snippets'] = '3';
+		$params['hl.fragsize'] = '50';
+		$params['sort'] = $sortby;
+		$params['spellcheck.onlyMorePopular'] = 'true';
+		$params['spellcheck.extendedResults'] = 'false';
+		$params['spellcheck.collate'] = 'true';
+		$params['spellcheck.count'] = '1';
+		$params['spellcheck'] = 'true';
+
+		//if ($facet_on_tags) {
+		//	$number_of_tags = $options['mss_max_display_tags'];
+		//	$params['f.tags.facet.limit'] = $number_of_tags;
+		//}
+
+		$response = $solr->search($qry, $offset, $count, $params);
+		if ( ! $response->getHttpStatus() == 200 ) {
+			$response = NULL;
+		}
+	}
+	return $response;
+}
+
+function mss_autocomplete($q, $limit) {
+	$options = mss_get_option();
+
+	$solr = new Mss_Solr();
+	if ($solr->connect($options, true)) {
+		$params = array();
+		$params['terms'] = 'true';
+		$params['terms.fl'] = 'spell';
+		$params['terms.lower'] = $q;
+		$params['terms.prefix'] = $q;
+		$params['terms.lower.incl'] = 'false';
+		$params['terms.limit'] = $limit;
+		$params['qt'] = '/terms';
+
+		$response = $solr->search($q, 0, $limit, $params);
+		if ( ! $response->getHttpStatus() == 200 ) {
+			return;
+		}
+
+		$terms = get_object_vars($response->terms->spell);
+		foreach($terms as $term => $count) {
+			printf("%s\n", $term);
+		}
+	}
+}
+
+
+?>
--- readme.txt
+++ readme.txt
@@ -4,61 +4,91 @@ Author URI: http://www.mysolrserver.com
 Plugin URI: http://wordpress.org/extend/plugins/advanced-search-by-my-solr-server/
 Tags: solr, search, search results, search integration, custom search, better search, search replacement, category search, comment search, tag search, page search, post search, search highlight, seo
 Requires at least: 3.0.0
-Tested up to: 3.2.1
-Stable tag: 1.0.2
+Tested up to: 3.3.1
+Stable tag: 2.0.0
 
-A WordPress plugin that help to manage Solr for WordPress plugin connexion to My Solr Server.
+
+A WordPress plugin that replaces the default WordPress search with a lot of benefits
 
 
 == Description ==
 
-Solr for WordPress plugin (http://wordpress.org/extend/plugins/solr-for-wordpress/) replaces the default WordPress search with a lot of benefits (see bellow). In order to make Solr for WordPress plugin work, you need a Solr server. If you don't have the time or resources to install, configure and maintain a Solr search server, My Solr Server do it for you ! 
+In order to make Solr for WordPress plugin work, you need a Solr server installed and configured with the provided schema.xml file. 
+If you don't have the time or resources to install, configure and maintain a Solr search server, My Solr Server do it for you ! 
+
 
 = What is My Solr Server ? =
 
-My Solr Server is a Software as a service Apache Solr enterprise search platform. My Solr Server provides hosted instances of the Apache Solr server. In a couple of minutes, you can create your own Solr server instance, manage it in our manager and integrate it with your favorite CMS by using its standart Solr plugin or module. Wordpress is one of the supported CMS.
+My Solr Server is a Software as a Service Apache Solr enterprise search platform. My Solr Server provides hosted instances of the Apache Solr server. In a couple of minutes, you can create your own Solr server instance, manage it in our manager and integrate it with your favourite CMS by using its standard Solr plugin or module. Wordpress is one of the supported CMS.
 
-My Solr Server plugin requieres you install Solr for WordPress plugin first.
 
-= What Solr for WordPress plugin does ? =
+= What Advanced Search by My Solr Server plugin does ? =
 
-Solr for WordPress plugin replaces the default WordPress search. Features and benefits include:
+Advanced Search by My Solr Server plugin replaces the default WordPress search. Features and benefits include:
 
-*	Index pages and posts
-*	Enable search and faceting on fields such as tags, categories, author, page type and custom fields.
-*	Treat the category facet as a taxonomy
-*	Add special template tags so you can create your own custom result pages to match your theme.
-*   Completely replaces default WordPress search, just install and configure.
+*   Index pages, posts and custom post types
+*   Enable search and faceting on fields such as tags, categories, author, page type, custom fields and custom taxonomies
+*   Add special template tags so you can create your own custom result pages to match your theme.
 *   Search term suggestions (AutoComplete)
 *   Provides better search results based on relevancy
 *   Create custom summarys with the search terms highlighted
 *   Completely integrated into default WordPress theme and search widget.
-*	Configuration options allow you to select pages to ignore, features to enable/disable, and what type of result information you want output.
-*   i18n Support
-*   Multisite support
+*   Configuration options allow you to select pages to ignore
 
 
 == Installation ==
 
 = Prerequisite = 
 
-Install and activate Solr for WordPress plugin (http://wordpress.org/extend/plugins/solr-for-wordpress/) 
+A Solr server installed and configured with the provided schema.xml file.
 
 = Installation =
 
 1. Upload the `advanced-search-by-my-solr-server` folder to the `/wp-content/plugins/` directory
 2. Activate the plugin through the 'Plugins' menu in WordPress
-3. Go in My Solr Server plugin settings page ("My Solr Server Settings") and configure the plugin with your My Solr Server username and password.
-4. Go in Solr for WordPress plugin settings page ("Solr Options") and load all your posts and/or pages via the "Load All Posts" button in the settings page.
+3. Go in Advanced Search by My Solr Server settings page ("Advanced Search by My Solr Server"), configure the plugin and Load your blog content in Solr ("Load Content" button)
 
 
 == Frequently Asked Questions ==
 
-= What version of WordPress does My Solr Server plugin work with? =
+= What version of WordPress does Advanced Search by My Solr Server plugin work with? =
 
-My Solr Server plugin works with WordPress 3.0.0 and greater.
+Advanced Search by My Solr Server plugin works with WordPress 3.0.0 and greater.
+
+= What version of Solr does Advanced Search by My Solr Server plugin work with? =
+
+Advanced Search by My Solr Server plugin works with Solr 1.4.x and 3.x
+
+= How to manage Custom Post type, custom taxonomies and custom fields
+
+Advanced Search by My Solr Server plugin is tested with "Custom Post Type UI" plugin for Custom Post type and custom taxonomies management and with "Custom Field Template" plugin for custom fields management
+
 
 == Screenshots ==
 
 1. Configuration Page
+
+
+== Changelog ==
+
+= 2.0.0 =
+
+* Includes all indexing and searching features
+* "Solr for Wordpress" plugin is not a pre-requisite anymore
+* Add support for custom post types and custom taxonomies
+* Settings page refactoring
+* Bug fixing
+
+= 1.0.2 =
+
+* Bug fixing
+
+= 1.0.1 =
+
+* Bug fixing
+
+= 1.0.0 =
+
+* Initial version just for My Solr Server connection management.
+* "Solr for Wordpress" plugin is a pre-requisite
 
